8b9a5bdd15cc904822640aa7f004a20165f2a686
Luis Fernando <luisfernando.mourag@gmail.com>
2025-06-02 12:53:55 -0300

    Sintaxe no Python

---
+++ sintaxe.py
@@ -0,0 +1,8 @@
+# coméntario em python
+"""
+coméntario em python de múltiplas linhas
+Este é um exemplo de código Python que demonstra a sintaxe básica.
+"""
+
+nome = "Shiro " + "Luna"
+print(nome)
