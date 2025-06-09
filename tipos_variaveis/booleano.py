1390000836f24bf24ccbee4f81a90a2b87a962f6
Luis Fernando <luisfernando.mourag@gmail.com>
2025-06-09 12:07:37 -0300

    Operadores lógicos

---
+++ tipos_variaveis/booleano.py
@@ -0,0 +1,23 @@
+# Condição Verdadeira
+True
+
+# Condição Falsa
+False
+
+# Condicionais
+if True:
+    print("A condição é verdadeira")
+if False:
+    print("A condição é falsa")
+
+# Operadores lógicos
+print(True and True)   # True
+print(True and False)  # False
+print(False and True)  # False
+print(False and False) # False
+# Logical OR
+print(True or True)    # True
+print(True or False)   # True
+print(False or True)   # True
+print(False or False)  # False
+print(not True)        # False
