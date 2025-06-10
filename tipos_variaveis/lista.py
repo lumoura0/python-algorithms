a4cb5fb3bd6717bd99e3b4ec5fe87a86b81dd7b3
Luis Fernando <luisfernando.mourag@gmail.com>
2025-06-10 11:00:31 -0300

    Listas em python

---
+++ tipos_variaveis/lista.py
@@ -0,0 +1,15 @@
+# Declaração
+
+minha_lista = [1, 2, 3, 4, 5, "texto", True, 3.14]
+# Acesso
+print(minha_lista[0])  # Acessa o primeiro elemento
+
+# Modificação
+minha_lista[0] = 10  # Modifica o primeiro elemento
+print(minha_lista)  # Exibe a lista modificada  
+# Adição
+minha_lista.append(6)  # Adiciona um novo elemento ao final da lista
+print(minha_lista)  # Exibe a lista após a adição
+print(minha_lista[-1])
+print(minha_lista[1:3])  # Exibe os elementos do índice 1 ao 2 (exclusivo)
+print(minha_lista[:6])  # Exibe os elementos do índice 1 até o final
