c70c9b9ef0a8e7e9392a4515a4acd5f21eddf54e
Luis Fernando <luisfernando.mourag@gmail.com>
2025-06-11 11:38:34 -0300

    Principais métodos do Dicionário

--- tipos_variaveis/dicionario.py
+++ tipos_variaveis/dicionario.py
@@ -15,3 +15,23 @@ print(dicionario["cidade"])  # Saída: São Paulo
 dicionario["profissao"] = "Engenheiro"
 # Acessando o novo valor
 print(dicionario["profissao"])  # Saída: Engenheiro
+
+# Removendo um par chave-valor
+del dicionario["idade"]
+
+print(dicionario)  # Saída: {'nome': 'Shiro', 'cidade': 'São Paulo', 'profissao': 'Engenheiro'}
+
+# Métodos: keys(), values(), items()
+chaves = dicionario.keys()  # Retorna as chaves do dicionário
+print(chaves)  # Saída: dict_keys(['nome', 'cidade', 'profissao'])
+print(list(chaves))  # Convertendo para lista: ['nome', 'cidade', 'profissao']
+
+# Método values() retorna os valores do dicionário
+valores = dicionario.values()  # Retorna os valores do dicionário
+print(valores)  # Saída: dict_values(['Shiro', 'São Paulo', 'Engenheiro'])
+
+# Método items() retorna os pares chave-valor do dicionário
+itens = list(dicionario.items())  # Retorna os pares chave-valor do dicionário
+print("Primeira chave-valor: %s = %s" % (itens[0][0],itens[0][1]))
+# Saída: Primeira chave-valor: nome = Shiro
+print("Segunda chave-valor: %s = %s" % (itens[1][0],itens[1][1]))
