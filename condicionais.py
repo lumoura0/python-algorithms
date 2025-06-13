438eb32360cc425a1b1e6fd41c35bcc267e055c3
Luis Fernando <luisfernando.mourag@gmail.com>
2025-06-13 14:03:35 -0300

    Condicionais em python

---
+++ condicionais.py
@@ -0,0 +1,24 @@
+# Condição
+# if, else, elif
+idade = 19
+if idade < 18:
+    print("Menor de idade") 
+elif idade == 18:
+    print("Tem 18 anos")
+else:
+    print("Maior de idade")
+
+if idade <= 18:
+    print("Menor de idade")
+
+mensagem = "Menor de idade" if idade < 18 else "Maior de idade"     
+print(mensagem)
+# Operadores lógicos    
+# and, or, not
+idade = 19
+if idade < 18 and idade > 0:
+    print("Menor de idade") 
+if idade < 18 or idade > 60:
+    print("Menor de idade ou maior de 60 anos") 
+if not idade < 18:
+    print("Maior de idade")
