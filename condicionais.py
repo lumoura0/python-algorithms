ae1ed011662907021bf7f6a17a48350ca3206e94
Luis Fernando <luisfernando.mourag@gmail.com>
2025-06-16 11:25:32 -0300

    Entrada de dados

--- condicionais.py
+++ condicionais.py
@@ -22,3 +22,10 @@ if idade < 18 or idade > 60:
     print("Menor de idade ou maior de 60 anos") 
 if not idade < 18:
     print("Maior de idade")
+
+
+mens = int(input("Digite um número: "))
+if mens % 2 == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")
