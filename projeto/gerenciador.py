34e9cd0f7edb095a4738e1043531e492e28a0993
Luis Fernando <luisfernando.mourag@gmail.com>
2025-06-18 01:45:16 -0300

    Funções do projeto - Visualizando Tarefas

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -1,30 +1,40 @@
-
 def adicionar_tarefa(tarefas, nome_tarefa):
-    tarefa = {
-        'tarefa': nome_tarefa,
-        'completada': False
-    }
-    tarefas.append(tarefa)
-    print(f"Tarefa '{nome_tarefa}' adicionada com sucesso!")
-    return
+  # tarefa: nome da tarefa
+  # completada: indicar se essa tarefa ja foi completada ou nao
+  tarefa = {"tarefa": nome_tarefa, "completada": False}
+  tarefas.append(tarefa)
+  print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+  return
+
+def ver_tarefas(tarefas):
+  print("\nLista de tarefas:")
+  for indice, tarefa in enumerate(tarefas, start=1):
+    status = "✓" if tarefa["completada"] else " "
+    nome_tarefa = tarefa["tarefa"]
+    print(f"{indice}. [{status}] {nome_tarefa}")
+  return
+
 
 tarefas = []
 while True:
-    print("\nMenu do Gerenciador de Lista de Tarefas:")
-    print("1. Adicionar Tarefa")
-    print("2. Ver Tarefas")
-    print("3. Atualizar Tarefa")
-    print("4. Completar Tarefa")
-    print("5. Sair")
-    tarefas = []
+  print("\nMenu do Gerenciador de Lista de tarefas:")
+  print("1. Adicionar tarefa")
+  print("2. Ver tarefas")
+  print("3. Atualizar Tarefas")
+  print("4. Completar Tarefas")
+  print("5. Deletar tarefas completadas")
+  print("6. Sair")
 
-    escolha = input("Escolha uma opção (1-5): ")
+  escolha = input("Digite a sua escolha: ")
 
-    if escolha == '1':
-        nome_tarefa = input("Digite o nome da tarefa: ")
-        adicionar_tarefa(tarefas, nome_tarefa)
-    elif escolha == '5':
-        print("Saindo do programa...")
-        break
+  if escolha == "1":
+    nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+    adicionar_tarefa(tarefas, nome_tarefa)
 
-print("Programa encerrado.")
+  elif escolha == "2":
+    ver_tarefas(tarefas)
+
+  elif escolha == "6":
+    break
+
+print("Programa finalizado")
