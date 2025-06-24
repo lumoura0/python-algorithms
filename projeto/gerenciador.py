57493175d269a277db01191a9d03a0087fe0b8e8
Luis Fernando <luisfernando.mourag@gmail.com>
2025-06-24 16:15:03 -0300

    Funções do projeto - Atualizar tarefas

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -14,6 +14,17 @@ def ver_tarefas(tarefas):
     print(f"{indice}. [{status}] {nome_tarefa}")
   return
 
+def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome):
+    print(f"Tarefa {indice_tarefa + 1} atualizada para '{novo_nome}'")
+    # Verifica se o índice da tarefa é válido
+    if 0 <= indice_tarefa < len(tarefas):
+        tarefas[indice_tarefa]["tarefa"] = novo_nome
+        print(f"Tarefa {indice_tarefa + 1} atualizada para '{novo_nome}'")
+    else:
+        print("Índice inválido. Tarefa não encontrada.")
+
+    return
+
 
 tarefas = []
 while True:
@@ -30,10 +41,14 @@ while True:
   if escolha == "1":
     nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
     adicionar_tarefa(tarefas, nome_tarefa)
-
   elif escolha == "2":
     ver_tarefas(tarefas)
-
+  elif escolha == "3":
+    ver_tarefas(tarefas)
+    # Solicita o índice da tarefa a ser atualizada
+    indice_tarefa = int(input("Digite o número da tarefa que deseja atualizar: ")) - 1
+    novo_nome = input("Digite o novo nome da tarefa: ")
+    atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
   elif escolha == "6":
     break
 
