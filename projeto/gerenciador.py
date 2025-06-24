087b370be7b9113c8f957bfa4e7f64a2134c266d
Luis Fernando <luisfernando.mourag@gmail.com>
2025-06-24 19:04:27 -0300

    Funções do projeto - Completar tarefas

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -22,7 +22,14 @@ def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome):
         print(f"Tarefa {indice_tarefa + 1} atualizada para '{novo_nome}'")
     else:
         print("Índice inválido. Tarefa não encontrada.")
+    return
 
+def completar_tarefa(tarefas, indice_tarefa):
+    if 0 <= indice_tarefa < len(tarefas):
+        tarefas[indice_tarefa]["completada"] = True
+        print(f"Tarefa {tarefas[indice_tarefa]['tarefa']} marcada como completada.")
+    else:
+        print("Índice inválido. Tarefa não encontrada.")
     return
 
 
@@ -49,6 +56,11 @@ while True:
     indice_tarefa = int(input("Digite o número da tarefa que deseja atualizar: ")) - 1
     novo_nome = input("Digite o novo nome da tarefa: ")
     atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
+  elif escolha == "4":
+    ver_tarefas(tarefas)
+    # Solicita o índice da tarefa a ser completada
+    indice_tarefa = int(input("Digite o número da tarefa que deseja completar: ")) - 1
+    completar_tarefa(tarefas, indice_tarefa)
   elif escolha == "6":
     break
 
